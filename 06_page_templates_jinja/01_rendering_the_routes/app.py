1f03e8fa0809339b5c44316d83129d9db4cf531b
csulva <chrissulva1@gmail.com>
2021-10-05 21:18:37 -0400

    edits to Flask labs

--- 06_page_templates_jinja/01_rendering_the_routes/app.py
+++ 06_page_templates_jinja/01_rendering_the_routes/app.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def welcome():
-    return "Welcome to Emily's Dog Costumes! See my <a href=\"/services\">"\
-        "services</a> and current line of <a href=\"/costumes\">costumes</a>"
+    return render_template('index.html')
+
+
+    # "Welcome to Emily's Dog Costumes! See my <a href=\"/services\">"\
+    #     "services</a> and current line of <a href=\"/costumes\">costumes</a>"
 
 
 @app.route('/services')
 def services():
-    return "I offer custom made costumes for your precious canine companion, "\
-        "and a free in-home consultation, to get the measurements."
+    return render_template('services.html')
+
+    # "I offer custom made costumes for your precious canine companion, "\
+    #     "and a free in-home consultation, to get the measurements."
 
 
 @app.route('/costumes')
@@ -44,7 +50,35 @@ def costumes():
             'image': '/static/witch.jpg',
         },
     ]
+    return render_template('costumes.html', costumes_list=costumes)
 
     # Emily wants to list her costumes above (listed above)
     # for now she put a placeholder
-    return f"Check out my costumes!"
+
+costumes_list = [
+        {
+            'header': 'Skeleton',
+            'body': "This style is great if you are feeling something extra "\
+                    "spooky for your pooch.",
+            'image': '/static/skeleton.jpg',
+        },
+        {
+            'header': 'Dracula',
+            'body': "My friend Judy got a new puppy, and I just had to dress "\
+                    "him up for Halloween! Super cute, and no coffins required.",
+            'image': '/static/dracula.jpg',
+        },
+        {
+            'header': 'Punk Rocker',
+            'image': '/static/punk-rocker.jpg',
+            'body': "I thought I was a huge Misfits fan, until I dressed "\
+                    "my bff's dog Frank. I think he pulls off the punk "\
+                    "rocker look better than me!",
+        },
+        {
+            'header': 'Witch',
+            'body': "Our dog Rose went with the husband out fishing, so I had "\
+            "to dress the cat up in this bewitching costume.",
+            'image': '/static/witch.jpg',
+        },
+    ]
